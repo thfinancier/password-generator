6c5ce63c312d0215729a16695c2cc92e2e05c2d6
The Financier <itisenward@gmail.com>
2023-11-05 19:16:03 +0100

    feat: add options (programs)

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Command } = require('commander')
+const program = new Command()
+
+program.version('1.0.0').description('Simple Password Generator')
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to passwords.txt')
+    // With these two lines below symbols and numbers are by default true
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse()
+
+const { length, save, numbers, symbols } = program.opts()
