2a0695267d51319a8073863d06aa51260a179717
The Financier <itisenward@gmail.com>
2023-11-05 21:19:18 +0100

    feat: save generated passwords to a passwords.txt file

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import chalk from 'chalk'
 import clipboardy from 'clipboardy'
 import { Command } from 'commander'
-import createPassword  from './utils/createPassword.js'
+import createPassword from './utils/createPassword.js'
+import savePassword from './utils/savePassword.js'
 const program = new Command()
 
 program.version('1.0.0').description('Simple Password Generator')
 
 program
-    .option('-l, --length <number>', 'length of password', '8')
-    .option('-s, --save', 'save password to passwords.txt')
-    // With these two lines below symbols and numbers are by default true
-    .option('-nn, --no-numbers', 'remove numbers')
-    .option('-ns, --no-symbols', 'remove symbols')
-    .parse()
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to passwords.txt')
+  // With these two lines below symbols and numbers are by default true
+  .option('-nn, --no-numbers', 'remove numbers')
+  .option('-ns, --no-symbols', 'remove symbols')
+  .parse()
 
 const { length, save, numbers, symbols } = program.opts()
 
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols)
+
+// Save password to a passwords.txt file
+if (save) {
+  savePassword(generatedPassword)
+}
 
 // Copy to clipboard
 clipboardy.writeSync(generatedPassword)
---
+++ utils/savePassword.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import os from 'os'
+import chalk from 'chalk'
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
+
+export default function savePassword(password) {
+  fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
+    fs.write(id, password + os.EOL, null, 'utf-8', () => {
+      fs.close(id, () => {
+        console.log(chalk.green('Password saved to passwords.txt'))
+        console.log(__dirname)
+      })
+    })
+  })
+}
